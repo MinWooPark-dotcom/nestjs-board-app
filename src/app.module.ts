5e4a03b9472300794fdf7462de86e9aa43a86e51
Minwoo Park <positive1attraction@gmail.com>
2022-04-29 20:55:00 +0900

    APPLY typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 // 애플리케이션의 루트(root) 모듈
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
+import { typeORMConfig } from './configs/typeorm.config';
 
 // @ 데코레이터는 클래스에 함수 기능을 추가한다.
 @Module({
-  imports: [BoardsModule],
-  })
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
+})
 export class AppModule {}
