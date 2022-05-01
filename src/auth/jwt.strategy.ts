3f105d741674802c9f62f1ca3abe5d0b0e92d912
Minwoo Park <positive1attraction@gmail.com>
2022-05-01 20:04:25 +0900

    APPLY configurations

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
+import * as config from 'config';
 
 @Injectable()
 // Nest.js can inject it anywhere this service is needed
@@ -14,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     private userRepository: UserRepository,
   ) {
     super({
-      secretOrKey: 'Secret1234',
+      secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
